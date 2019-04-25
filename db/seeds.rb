3ab717aeb1bd7acdd79a1c3379b66b853b09b6d3
Artika Busa <busaartika@gmail.com>
2019-04-25 16:45:51 +0530

    db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,12 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 AdminUser.create!(email: 'admin@example.com', password: 'password')
+Brand.create!([{ name: 'Apple' }, { name: 'Samsung' }, { name: 'Oppo' },
+               { name: 'Vivo' }, { name: 'Mi' }])
+Color.create!([{ name: 'Gold' }, { name: 'Silver' }, { name: 'Black' },
+               { name: 'Blue' }, { name: 'Red Rose' }])
+Category.create!([{ name: 'Touch Screen' }, { name: 'Smart Phone' }])
+Processor.create!([{ name: 'iOS' }, { name: 'Android' }])
+Storage.create!([{ name: '16 GB' }, { name: '32 GB' }, { name: '64 GB' }])
+Display.create!([{ name: '4 inche' }, { name: '4.5 inche' }, { name: '5 inche' },
+                 { name: '5.5 inche' }])
